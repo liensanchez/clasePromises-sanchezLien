aafb10c9d2b2bba7f75be640fd8138fc8aa4938e
Lien! <80282938+liensanchez@users.noreply.github.com>
2022-03-24 20:19:27 -0300

    Item

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,5 @@
+const productos  = [
+    {id:'1', modelo:'iphone7'},
+    {id:'2', modelo:'iphone7plus'},
+    {id:'3', modelo:'iphone8'}
+    ]
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {useState} from 'react'
+import {useState} from 'react';
+import Item from 'ItemList/Item';
 
 
 
@@ -11,7 +12,7 @@ const task = new Promise ( (resolve, reject) => {
 
   if (condition){
 
-    resolve('200 ok')
+    resolve(productos)
 
   }else{
 
@@ -25,64 +26,28 @@ const task = new Promise ( (resolve, reject) => {
 
 function ItemListContainer({greeting}) {
 
-  const [productos, setProductos]=useState(0)
+  setTimeout(() => {
+    task
 
-  //let productos=0
+      .then (resp => console.log (resp))
+      //resp lo hacemos nosotros puede ser resp, data, etc 
 
-  const sumaProductos =()=>{
+      .catch(err => console.log(err) )
 
-    //productos ++
+      .then () //en cada then hago una sola tarea
 
-    //console.log(productos)
-
-    //ponemos un limite en la cantidad de produtos
-    if (productos==10){
-
-      //sumamos 0 a partir del 10
-      setProductos(productos+0)
-
-    }else{
-
-      //mientras no lleguemos a 10 nos suma +1
-      setProductos(productos+1)
-
-    }
-
-  } 
+      .finally (() => console.log ('siempre a lo ultimo') ) 
 
 
-
-  const restaProductos=()=>{
-
-    setProductos(productos-1)
-
-  }
-
-
-  task
-
-  .then (resp => console.log (resp))
-  //resp lo hacemos nosotros puede ser resp, data, etc 
-
-  .catch(err => console.log(err) )
-
-  .then () //en cada then hago una sola tarea
-
-  .finally (() => console.log ('siempre a lo ultimo') ) 
-
+  }, 3000 );
+  
 
 
   return (
 
     <>
 
-    <h2>{greeting} </h2>
-
-    <button onClick={restaProductos}>-1</button>
-
-    <label>{productos} </label>
-
-    <button onClick={sumaProductos}>+1</button>
+    
 
     </>
     
