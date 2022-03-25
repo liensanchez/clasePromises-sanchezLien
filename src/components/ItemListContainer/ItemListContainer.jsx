51ed48c7d17a7d716cdc0737a4760ca9bfd822dc
Lien Sanchez <liensilviosanchez@gmail.com>
2022-03-25 15:04:59 -0300

    import productos

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {useState} from 'react';
-import Item from 'ItemList/Item';
+import productos from '../Item/Item'
 
 
 
